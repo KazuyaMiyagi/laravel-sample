c2df1cc23cefcdf42bf4e5b9d57380e2136cdcac
Kazuya Miyagi <umi.kukki@gmail.com>
2021-04-13 15:23:33 +0900

    Update Container bulder image

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -23,7 +23,7 @@ resource "aws_codebuild_project" "container_builder" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:2.0"
+    image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
