628f82f18f27703d5bdf5643dbe0d98bde1574ef
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2021-08-26 16:02:23 +0900

    Use Terraform v0.14.11

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+# https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
+# terraform settings
+terraform {
+  required_version = "~> 0.14.11"
+}
+
 # https://www.terraform.io/docs/providers/aws/d/caller_identity.html
 # account data
 data "aws_caller_identity" "current" {}
