7c4b99c945a22adfe99fdde502b3a195a9804da8
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2021-08-26 16:28:15 +0900

    terraform fmt

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -6,9 +6,9 @@ resource "aws_ecs_cluster" "main" {
   }
   configuration {
     execute_command_configuration {
-      logging    = "OVERRIDE"
+      logging = "OVERRIDE"
       log_configuration {
-        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_execute_command.name
+        cloud_watch_log_group_name = aws_cloudwatch_log_group.ecs_execute_command.name
       }
     }
   }
